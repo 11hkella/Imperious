01a2f1a0931deb7fb5b332adbee919156dbcdc38
11hkella <henry.a.kellam@gmail.com>
2025-06-05 20:29:01 -0500

    code cleanup and formatting

--- src/resources.ts
+++ src/resources.ts
@@ -1,14 +1,9 @@
 import { ImageSource, Loader } from "excalibur";
 
-// It is convenient to put your resources in one place
+// Resources are a way to load and manage assets in Excalibur
 export const Resources = {
-  Sword: new ImageSource("./images/sword.png") // Vite public/ directory serves the root images
-} as const; // the 'as const' is a neat typescript trick to get strong typing on your resources. 
-// So when you type Resources.Sword -> ImageSource
-
-// We build a loader and add all of our resources to the boot loader
-// You can build your own loader by extending DefaultLoader
+  Sword: new ImageSource("./images/sword.png"),
+};
 export const loader = new Loader();
-for (const res of Object.values(Resources)) {
-  loader.addResource(res);
-}
+
+loader.addResources(Object.values(Resources));
